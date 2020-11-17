145149f68353702304e919540d8029e254cd441b
Hayden <37120379+haydenwalker980@users.noreply.github.com>
2020-11-17 09:51:57 -0800

    Update database.js

--- configs/database.js
+++ configs/database.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    uri: '',
+    uri: 'mongodb+srv://mlist:mongolist@pleasefuckingsendhelp.eo0pz.mongodb.net/reddit-app?retryWrites=true&w=majority',
     options: {
         dbName: 'reddit-app',
         useNewUrlParser: true
